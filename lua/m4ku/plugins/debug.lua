e03db2bb36e3f1fdc935384b46fc1d214bcd8e46
M4kU <80482255+Sir-M4kU@users.noreply.github.com>
2024-06-05 12:16:25 -0500

    setting up the debugger

--- lua/m4ku/plugins/debug.lua
+++ lua/m4ku/plugins/debug.lua
@@ -11,36 +11,73 @@ return {
     "jay-babu/mason-nvim-dap.nvim"
   },
   config = function()
+    local mason_registry = require("mason-registry")
+    mason_registry.refresh()
+    local js_debug = mason_registry.get_package("js-debug-adapter")
     local dap = require("dap")
     local dapui = require("dapui")
+    local js_debug_path = js_debug:get_install_path() .. "/js-debug/src/dapDebugServer.js"
 
     require("mason-nvim-dap").setup({
+      ensure_installed = { "js-debug-adapter", "codelldb" },
       automatic_installation = true,
-      handlers = {},
-      ensure_installed = {}
+      handlers = {}
     })
 
-    dapui.setup({
-      icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
-      controls = {
-        icons = {
-          pause = '⏸',
-          play = '▶',
-          step_into = '⏎',
-          step_over = '⏭',
-          step_out = '⏮',
-          step_back = 'b',
-          run_last = '▶▶',
-          terminate = '⏹',
-          disconnect = '⏏',
+    if not dap.adapters["pwa-node"] then
+      dap.adapters["pwa-node"] = {
+        type = "server",
+        host = "localhost",
+        port = "${port}",
+        executable = {
+          command = "node",
+          args = { js_debug_path, "-p", "${port}" }
         }
       }
-    })
+    end
+    if not dap.adapters["node"] then
+      dap.adapters["node"] = function(cb, config)
+        if config.type == "node" then
+          config.type = "pwa-node"
+        end
 
-    map("5", dap.continue, "Start/Continue")
-    map("1", dap.step_into, "Step into")
-    map("2", dap.step_over, "Step over")
-    map("3", dap.step_out, "Step Out")
+        local native_adapter = dap.adapters["pwa-node"]
+        if type(native_adapter) == "function" then
+          native_adapter(cb, config)
+        else
+          cb(native_adapter)
+        end
+      end
+    end
+
+    for _, lang in ipairs({ "typescript", "javascript", "typescriptreact", "javascriptreact" }) do
+      if not dap.configurations[lang] then
+        dap.configurations[lang] = {
+          {
+            type = "pwa-node",
+            request = "launch",
+            name = "Launch file",
+            program = "${file}",
+            cwd = "${workspaceFolder}"
+          },
+          {
+            type = "pwa-node",
+            request = "attach",
+            name = "Attach",
+            processId = require("dap.utils").pick_process,
+            cwd = "${workspaceFolder}"
+          }
+        }
+      end
+    end
+
+    dapui.setup()
+
+    map("<leader>td", dapui.toggle, "Toggle UI")
+    map("<F5>", dap.continue, "Start/Continue")
+    map("<F1>", dap.step_into, "Step into")
+    map("<F2>", dap.step_over, "Step over")
+    map("<F3>", dap.step_out, "Step Out")
     map("<leader>b", dap.toggle_breakpoint, "Toggle breakpoint")
     map(
       "<leader>B",
@@ -51,7 +88,7 @@ return {
     )
 
 
-    map("7", dapui.toggle, "See last session result")
+    map("<F7>", dapui.toggle, "See last session result")
 
     local dapui_config = "dapui_config"
 
